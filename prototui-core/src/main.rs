9d8c9c8b02aba4333d3c613e37dae970105a633f
Philipp Reiter <philipp.reiter@freiheit.com>
2023-04-20 16:30:32 +0200

    add new test service with repeated fields

--- prototui-core/src/main.rs
+++ prototui-core/src/main.rs
@@ -1,4 +1,5 @@
 use http::Uri;
+use prost_reflect::{ReflectMessage, Value};
 use prototui_core::{call_unary_blocking, Result};
 use prototui_core::{init, ProtoDescriptor};
 
@@ -10,14 +11,29 @@ fn main() -> Result<()> {
     for service in services.clone() {
         println!("{:?}", service.name());
     }
-    let service = &services[1];
+    let service = &services[2];
     let methods = desc.get_methods(service);
-    let method = methods.last().unwrap();
-    let req = desc.get_request(method);
+    let method = &methods[0];
+    let req = desc.get_response(method);
     println!("{:?}", method.name());
+    // println!("{:?}", method);
     println!("{:?}", req.message_name());
     println!("{:?}", req.to_json());
+    // println!("{:?}", req);
 
+    for field in req.get_message_descriptor().fields() {
+        println!("FIELD {:?}", field.name());
+        let value = Value::default_value_for_field(&field);
+        println!("VALUE {:?}", value);
+        let message = value.as_message().unwrap();
+        println!("MESSAGE {:?}", message.to_text_format());
+        let desc = message.descriptor();
+        for inner in desc.fields() {
+            println!("FIELD {:?}", inner.name());
+            let value = Value::default_value_for_field(&inner);
+            println!("VALUE {:?}", value);
+        }
+    }
     // send message to gRPC server
     let uri = Uri::from_static("http://localhost:50051");
     call_unary_blocking(&cfg, uri, &req)?;
