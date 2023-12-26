60aa0cec27f4de10c0050698ff005db6385b06d5
Philipp Reiter <philipp.reiter@freiheit.com>
2023-12-26 13:08:12 +0100

    Clean up main

--- stellarpc-core/src/main.rs
+++ stellarpc-core/src/main.rs
@@ -1,19 +1,48 @@
+use http::Uri;
+use stellarpc_core::client::async_call_unary;
+use stellarpc_core::client::create_runtime;
 use stellarpc_core::ProtoDescriptor;
 use stellarpc_core::Result;
 
 fn main() -> Result<()> {
-    // get services, methods and a request message
+    _test_async()?;
+
+    Ok(())
+}
+fn _test_async() -> Result<()> {
     let desc = ProtoDescriptor::new(
-        // vec!["/Users/philippreiter/Rust/stellarpc/test_utils"],
+        vec!["/Users/philippreiter/Rust/stellarpc/test_utils"],
+        vec!["grpc_simple/debugger.proto"],
+    )?;
+    // Services and methods
+    let service = &desc.get_services()[0];
+    let method = &desc.get_methods(&service)[1];
+
+    // Request
+    let mut req = desc.get_request(&method);
+    req.message_mut().apply_template();
+    req.set_address("http://localhost:50051");
+    req.insert_metadata("key", "value")?;
+
+    // Call grpc
+    let rt = create_runtime()?;
+    let handle = rt.spawn(async move {
+        let uri = Uri::try_from(req.address()).unwrap();
+        let resp = async_call_unary(uri, &req).await;
+        println!("Response {:?}", resp);
+    });
+    let _ = rt.block_on(handle);
+
+    println!("Shutdown");
+    rt.shutdown_background();
+
+    return Ok(());
+}
+
+fn _test_oneof() -> Result<()> {
+    let desc = ProtoDescriptor::new(
         vec!["/Users/philippreiter/Rust/stellarpc/stellarpc-core"],
-        vec![
-            // "test_files/recursive.proto",
-            "test_files/oneof.proto",
-            // "grpc_simple/greeter.proto",
-            // "grpc_simple/timekeeper.proto",
-            // "grpc_simple/debugger.proto",
-            // "grpc_simple/productfinder.proto",
-        ],
+        vec!["test_files/oneof.proto"],
     )?;
     let services = desc.get_services();
     for service in services.clone() {
@@ -25,36 +54,5 @@ fn main() -> Result<()> {
 
     req.message_mut().apply_template();
     println!("{:?}", req.message().to_json());
-    // for method in desc.get_methods(service) {
-    //     println!("{:?}", method.name());
-    // }
-    // let method = &methods[0];
-    // let mut req = desc.get_request(method);
-    // println!("{:?}", method.name());
-    // println!("{:?}", method);
-    // println!("{:?}", req.message_name());
-    // println!("{:?}", req.message().to_json());
-    // println!("{:?}", req);
-
-    // for field in req.get_message_descriptor().fields() {
-    //     println!("FIELD {:?}", field.name());
-    //     let value = Value::default_value_for_field(&field);
-    //     println!("VALUE {:?}", value);
-    //     let message = value.as_message().unwrap();
-    //     println!("MESSAGE {:?}", message.to_text_format());
-    //     let desc = message.descriptor();
-    //     for inner in desc.fields() {
-    //         println!("FIELD {:?}", inner.name());
-    //         let value = Value::default_value_for_field(&inner);
-    //         println!("VALUE {:?}", value);
-    //     }
-    // }
-    // // send message to grpc server
-    // req.set_address("http://localhost:50051");
-    // req.insert_metadata("metadata-key", "metadata-value")
-    //     .unwrap();
-    // let resp = call_unary_blocking(&req)?;
-    // println!("{:?}", resp.message.to_json());
-
-    Ok(())
+    return Ok(());
 }
