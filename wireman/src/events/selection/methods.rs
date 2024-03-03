8a33253f54080958fc250750c16f30faff2e9650
Philipp Reiter <p.reiter93@gmail.com>
2024-03-03 12:14:25 +0100

    fix

--- wireman/src/events/selection/methods.rs
+++ wireman/src/events/selection/methods.rs
@@ -30,6 +30,8 @@ impl fmt::Display for MethodsSelectionEvents {
     }
 }
 
+pub struct MethodsSelectionEventsHandler;
+
 impl EventHandler for MethodsSelectionEventsHandler {
     type Context = AppContext;
 
