b6408b7e407416465b504e010db4964d3cb71f6a
Philipp Reiter <p.reiter93@gmail.com>
2024-09-17 11:42:35 +0200

    wireman(core): Remove must use arguments

--- wireman-core/src/client/tls.rs
+++ wireman-core/src/client/tls.rs
@@ -8,13 +8,11 @@ pub struct TlsConfig(pub(super) ClientTlsConfig);
 
 impl TlsConfig {
     /// Create a new `TlsConfig` with native certificate.
-    #[must_use]
     pub fn native() -> Result<Self> {
         Ok(Self(ClientTlsConfig::new().with_enabled_roots()))
     }
 
     /// Create a new `TlsConfig` with a custom certificate.
-    #[must_use]
     pub fn custom(cert_path: String) -> Result<Self> {
         let pem = std::fs::read_to_string(cert_path).map_err(Error::LoadTLSCertificateError)?;
         let ca = Certificate::from_pem(pem);
