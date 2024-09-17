6d9de5ce26e738f850f1d515b833af52628e86b7
Philipp Reiter <p.reiter93@gmail.com>
2024-09-17 10:33:54 +0200

    wireman-core: Upgrade to tonic v0.12 and http 1

--- wireman-core/src/client/tls.rs
+++ wireman-core/src/client/tls.rs
@@ -1,83 +1,25 @@
 #![allow(clippy::module_name_repetitions)]
 use crate::error::{Error, Result};
-use hyper::client::HttpConnector;
-use hyper_rustls::HttpsConnector;
-use hyper_rustls::HttpsConnectorBuilder;
-use rustls::pki_types::CertificateDer;
-use rustls::RootCertStore;
-use serde::{Deserialize, Serialize};
+use tonic::transport::{Certificate, ClientTlsConfig};
 
 /// The TLS config of the grpc client.
-#[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq, Eq, PartialOrd)]
-pub struct TlsConfig {
-    /// Custom certificates
-    custom_cert: Option<String>,
-}
+#[derive(Debug, Clone)]
+pub struct TlsConfig(pub(super) ClientTlsConfig);
 
 impl TlsConfig {
-    /// Create a new `TlsConfig` with optional custom certificates.
+    /// Create a new `TlsConfig` with native certificate.
     #[must_use]
-    pub fn new(custom_cert: Option<String>) -> Self {
-        Self { custom_cert }
+    pub fn native() -> Result<Self> {
+        Ok(Self(ClientTlsConfig::new().with_enabled_roots()))
     }
 
-    /// Get the HTTPS connector based on the TLS configuration.
+    /// Create a new `TlsConfig` with a custom certificate.
     #[must_use]
-    pub fn get_connector_from_tls(&self) -> HttpsConnector<HttpConnector> {
-        let tls = self.get_client_config();
+    pub fn custom(cert_path: String) -> Result<Self> {
+        let pem = std::fs::read_to_string(cert_path).map_err(Error::LoadTLSCertificateError)?;
+        let ca = Certificate::from_pem(pem);
+        let tls = ClientTlsConfig::new().ca_certificate(ca);
 
-        let mut http = HttpConnector::new();
-        http.enforce_http(false);
-
-        HttpsConnectorBuilder::new()
-            .with_tls_config(tls)
-            .https_or_http()
-            .enable_http2()
-            .wrap_connector(http)
+        Ok(Self(tls))
     }
-
-    /// Get the client's TLS configuration as a `rustls::ClientConfig`.
-    pub fn get_client_config(&self) -> rustls::ClientConfig {
-        if let Some(cert) = &self.custom_cert {
-            return get_client_config_with_custom_certs(cert).unwrap();
-        }
-        get_client_config_native_certs()
-    }
-}
-
-/// Defaults TLS client config with native roots.
-fn get_client_config_native_certs() -> rustls::ClientConfig {
-    let root_store = RootCertStore {
-        roots: webpki_roots::TLS_SERVER_ROOTS.into(),
-    };
-    rustls::ClientConfig::builder()
-        .with_root_certificates(root_store)
-        .with_no_client_auth()
-}
-
-/// Get TLS client configuration with a custom certificate.
-fn get_client_config_with_custom_certs(ca_cert: &str) -> Result<rustls::ClientConfig> {
-    let roots = root_store_from_cert_file(ca_cert)?;
-    let config = rustls::ClientConfig::builder()
-        .with_root_certificates(roots)
-        .with_no_client_auth();
-    Ok(config)
-}
-
-// Load public certificate from file.
-fn root_store_from_cert_file(filename: &str) -> Result<RootCertStore> {
-    // Open certificate file.
-    let certfile = std::fs::File::open(filename).map_err(Error::LoadTLSCertificateError)?;
-    let mut reader = std::io::BufReader::new(certfile);
-
-    // Read the certificates
-    let certs: Vec<CertificateDer> = rustls_pemfile::certs(&mut reader)
-        .filter_map(|result| result.ok())
-        .collect();
-
-    // Parse the certificates
-    let mut roots = rustls::RootCertStore::empty();
-    roots.add_parsable_certificates(certs);
-
-    Ok(roots)
 }
