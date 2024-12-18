98fb827c0325b50d6e63fad9c2d0b02fc4e82541
Philipp Reiter <p.reiter93@gmail.con>
2024-12-18 14:40:28 +0100

    website: Update prettier settings

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,14 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		paths: {
-			base: process.env.BASE_PATH,
-		}
-	}
+    kit: {
+        adapter: adapter(),
+        paths: {
+            base: process.env.BASE_PATH
+        }
+    }
 };
 
 export default config;
